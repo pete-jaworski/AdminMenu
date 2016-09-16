d1d93a5bc8e3b86c3573910f89cbee7672645c65
Piotr <piotrjaworski@hotmail.com>
2016-09-16 12:29:07 +0200

    First commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Nauka_AdminMenu',
+        __DIR__
+    );
